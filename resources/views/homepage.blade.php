60acaa60b9f0f21e9ef6e2852f7355e702a81fb7
Owen Dawson <otbdawson@gmail.com>
2017-02-25 02:11:18 +0000

    Updated landing page
    text and alignment

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,7 +6,9 @@
 	<div class="right"></div>
 
 	<div class="login">
-		
+		<h3>Create an Account</h3>
+
+		<p>Enter your information below to create a skatespot account.</p>
 		@include('auth/homeregister')
 
 	</div>
@@ -18,6 +20,32 @@
 	<div class="top">
 		
 		<div class="logo"><img src="{{ asset('images/logo.png') }}" alt="Skatespot Logo"></div>
+
+		<ul>
+			<li><a href="#">Explore</a></li>
+			<li><a href=""> | </a></li>
+			<li><a href="#">Sign-in</a></li>
+		</ul>
+
+	</div>
+
+	<div class="hero">
+
+		<div class="content">
+
+			<h1>Join the biggest online skateboarding community today.</h1>
+
+			<p>Skatespot brings to you one of the first community based skateboarding networks, 
+along with hundreds of searchable online skatespots.
+<br>
+<br>
+We aim to provide an online social experience with hundreds of features made especially for skateboarders.
+
+</p>
+
+			<button><a href="#">Learn More</a></button>
+
+		</div>
 
 	</div>
 
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,5 +3,6 @@
 <head>
 	<title>skatespot</title>
 	<link href="{{ asset('css/master.css') }}" rel="stylesheet">
+	<link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v=2" >
 </head>
 <body>
